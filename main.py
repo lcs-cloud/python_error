f0b882f37d0e5ec1fc0dd5046d3d81dffce019b4
lcs-cloud <59048243+lcs-cloud@users.noreply.github.com>
2020-01-20 15:43:50 +0800

    增加文件：main.py，requirements.txt
    增加文件，错误的代码

---
+++ main.py
@@ -0,0 +1,16 @@
+#coding=utf-8  
+__author__ = 'cloudtogo'
+
+from flask import Flask
+import sys
+
+
+
+#reload(sys)
+#sys.setdefaultencoding('utf8')
+
+def hello():
+    return '恭喜您成功地发布了一个运行在云端的python程序 hello-Github-lcs-cloud'
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0',port=8080)
